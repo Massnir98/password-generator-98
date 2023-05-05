8705742746be5c4cd9be0f06cd0199fb9457358e
massinir98 <renan.mmelo@hotmail.com>
2023-05-04 21:08:08 -0400

    Last-Step

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,72 @@
-// Assignment code here
+// Assignment 3
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var password = generatePassword();
+var passwordText = document.querySelector('#password');
+passwordText.value = password;
+}
 
-  passwordText.value = password;
+// Generate password 
+function generatePassword() {
+var length = parseInt(prompt("How long do you want your password? (Choose between 8 and 50)"));
 
+// Input is a number between 8 and 50
+if (isNaN(length) || length < 8 || length > 50) {
+alert("Please choose a valid number between 8 and 50.");
+return "";
+}
+
+var uppercase = confirm("Would you like to include lowercase letters?");
+var lowercase = confirm("Would you like to include uppercase letters?");
+var numbers = confirm("Would you like to include numbers?");
+var specialChars = confirm("Would you like to include special characters?");
+
+// At least one special character must be selected
+if (!uppercase && !lowercase && !numbers && !specialChars) {
+alert("Please select at least one character type.");
+return "";
+}
+
+var password = "";
+var charSet = "";
+
+// Add character types to character set
+if (uppercase) {
+charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+if (lowercase) {
+charSet += "abcdefghijklmnopqrstuvwxyz";
+}
+
+if (numbers) {
+charSet += "0123456789";
+}
+
+if (specialChars) {
+charSet += "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+}
+
+// Generate random password 
+for (var i = 0; i < length; i++) {
+password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+}
+
+return password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-var length = parseInt(prompt("How many characters would you like? (Choose between 8 and 20."))
-return "";
+generateBtn.addEventListener('click', writePassword);
 
-// Input is a number between 8 and 20
-if (isNaN(length) || length < 8 || length >20) 
-{ alert("Please choose a valid number between 8 and 20.");
-return "";
-}
+// Hover
+generateBtn.addEventListener('mouseover', function() {
+this.style.color = 'white';
+});
 
-var lowercase = confirm("Would you like to include lowercase letter to your password?")
-
-var uppercase = confirm("Would you like to include uppercaseletter to your password?")
-
-var specialChars = confirm("Would you like to include any special character to your password?");
-
-var numbers = confirm("Would you like to include numbers to your password?")
+generateBtn.addEventListener('mouseout', function() {
+this.style.color = '#4c4c4c';
+});
